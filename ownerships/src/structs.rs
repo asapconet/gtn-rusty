3c20d752399747f0428915ce177de1e0dd4887c0
ASAP_A1 <aaronrickymeek@gmail.com>
2026-01-08 12:46:47 +0100

    learn: methods

--- ownerships/src/structs.rs
+++ ownerships/src/structs.rs
@@ -1,15 +1,28 @@
 // PURE STRUCTS
+
+#[derive(Debug)]
 struct UserData {
     name: String,
-    age: u16,
+    age: u32,
     active: bool,
     gender: String,
-    number_of_followers: u64,
+    number_of_followers: u32,
+}
+
+struct UserLongevity {
+    age: u32,
+    number_of_followers: u32,
 }
 
 // TUPLE STRUCTS
-struct Physic(i32, String);
 struct ExchangeRate(i32, String);
+
+//METHODS
+impl UserLongevity {
+    fn longevity(&self) -> u32 {
+        self.age * self.number_of_followers / 5
+    }
+}
 
 pub fn run() {
     let new_user = UserData {
@@ -31,13 +44,12 @@ pub fn run() {
         ..new_user
     };
 
-    println!("updated user info:  {}", other_user.name);
+    println!("updated user info:  {:#?}", other_user);
 
     // the operation in other_user takes only the name key in the UserData struct
     // and that renders the new_user invalid as well as it values[can be read however].
     // Now for the rest of the values in the struct they remain unused until a mutate them.
 
-    let user_look = Physic(6, String::from("ft"));
     let todays_rate = ExchangeRate(2342, String::from("USD to AUS"));
 
     println!(
@@ -45,4 +57,18 @@ pub fn run() {
         todays_rate.1,
         todays_rate.0.to_string()
     );
+
+    let user_longevity = UserLongevity {
+        age: new_user.age,
+        number_of_followers: new_user.number_of_followers,
+    };
+
+    println!(
+        "the user is set to be online for {} mins",
+        user_longevity.longevity()
+    )
 }
+
+// Generally struct are like setting types or interfaces in js, but for rust this time
+// it remains strict as usual and everything must be allowed either for a read or write.
+// If mutable all must be and not just one, it shouts as in ts too when not all the keys and velues are used
