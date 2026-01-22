02739d3e05223480e504ac9427dbb60851a1705f
ASAP_A1 <aaronrickymeek@gmail.com>
2026-01-22 15:45:33 +0100

    perf: better usage for Option and

--- ownerships/src/enums.rs
+++ ownerships/src/enums.rs
@@ -11,50 +11,63 @@ enum TaskType {
 //this is more like a tuple enum
 #[derive(Debug)]
 enum TaskAnalysis {
-    id(u32),
-    is_task_info { id: u32, task_type: TaskType },
-    is_task_progress { id: u32, status: TaskStatus },
+    is_task_info {
+        id: Option<u32>,
+        task_type: TaskType,
+    },
+    is_task_progress {
+        id: Option<u32>,
+        status: TaskStatus,
+    },
+    is_task_analysis {
+        id: Option<u32>,
+        task_type: TaskType,
+        status: Option<TaskStatus>,
+    },
 }
 
 // basically enums are an advanced and less stressful way to represent data in Rust.
 // The take struct like features where you can just name an emnum and pass the types it expects,
 // kind of like nesting object types in .js
 
+// this should check which of the supplied task information is complete or not
 impl TaskAnalysis {
-    fn show(&self) {
-        if let TaskAnalysis::id(id) = self {
-            println!("Task ID: {}", id);
-        } else {
-            println!("no Task ID");
+    fn task_data_complete(&self) -> bool {
+        match self {
+            TaskAnalysis::is_task_info { id, task_type } => id.is_some(),
+            TaskAnalysis::is_task_progress { id, status } => id.is_some(),
+            TaskAnalysis::is_task_analysis {
+                id,
+                task_type,
+                status,
+            } => id.is_some() && status.is_some(),
         }
+    }
+
+    fn task_data_incomplete(&self) -> bool {
+        !self.task_data_complete()
     }
 }
 
 pub fn run() {
     let new_task = TaskAnalysis::is_task_info {
-        id: 34,
+        id: None,
         task_type: TaskType::URGENT,
     };
 
     let general_progress = TaskAnalysis::is_task_progress {
-        id: 1,
+        id: Some(34),
         status: TaskStatus::COMPLETED,
     };
 
-    let show_task_id = TaskAnalysis::id(55);
-    show_task_id.show();
-
-    //HOW TO USE OPTION -- not fully into the other rep of our bad native 'null' value.
-    let complete_task_info = Some(TaskAnalysis::from(new_task));
-    let no_task_info: Option<TaskAnalysis> = None;
-
-    match no_task_info {
-        Some(task) => println!("Task information: {:#?}", task),
-        None => println!("No task information provided"),
-    }
-
-    match complete_task_info {
-        Some(task) => println!("Task information: {:#?}", task),
-        None => println!("No task information provided"),
-    }
+    //HOW TO USE OPTION from the method to check if task info is complete
+    if new_task.task_data_complete() {
+        println!("New task info is complete");
+    } else if general_progress.task_data_complete() {
+        println!("General task progress information is ready");
+    } else if new_task.task_data_incomplete() {
+        println!("New task analysis information is incomplete");
+    } else {
+        println!("Either Task progress or analysis information is incomplete");
+    };
 }
